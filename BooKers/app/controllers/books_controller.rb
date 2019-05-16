8b6e050bcf8279f0b0c5c3c57db080b755d95f31
青木大輔 <aokidaisuke@aokidaisukenoMacBook-Pro.local>
2019-05-16 12:56:52 +0900

    Finish

--- BooKers/app/controllers/books_controller.rb
+++ BooKers/app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-  	flash[:notice] = "successfully"
+  	flash[:notice] = "Book was successfully created."
   	redirect_to book_path(book.id)
   end
 
@@ -26,13 +26,14 @@ class BooksController < ApplicationController
   def update
   	book =Book.find(params[:id])
   	book.update(book_params)
-  	flash[:notice] = "successfully"
+  	flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book)
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
+    flash[:notice] = "Book was successfully destroyed."
   	redirect_to books_path 
   end
 
